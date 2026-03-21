7b07103612789e7258afc6cafd8205e69b18cd8a
dustyspy <itsmainul04@gmail.com>
2026-03-21 23:49:15 +0600

    Update index.js

--- index.js
+++ index.js
@@ -13,12 +13,42 @@ import { wrapSendMessageGlobally } from './src/utils/typing.js';
 dotenv.config();
 
 // =======================
-// 🔥 FIREBASE INIT
+// 🔥 FIREBASE DIRECT CONFIG
 // =======================
-const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
-
-// 🔥 FIX PRIVATE KEY
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+const serviceAccount = {
+  "type": "service_account",
+  "project_id": "downloaderx-wa-code",
+  "private_key_id": "acf82fecbbde1f8110f85394dc97d2179db97e7b",
+  "private_key": `-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCqKq1KHBVsVSf2
+TYBd/tdeNQndyb5K+5paHEHvcS84OxJ/KgXiXI4h5QUHkz4fKOCeb3OQCa5bRNjc
+Sn/qvWr27PGJEzNFzH8qunNhImdhnPeKEkcLPecPti6OAbtiXpW2v3lwPShDKBBK
+6R4n315UljJEaMWT6QKC16U9UM2veXoGVrTvsnH0KCIbpT3p0d3mEij2vNJC4G6k
+af7h/hVNelDLeAvyKYTSeJp3WMDkkcMAJtAZr9de5SuxJJSE9gzvECxTPrkjgvnU
+A8hNWvG9daP/FxUAEkVpABzsgml41j7onrx7uYOy+VdlxAa6IwhC5uuUU+NYmLtD
+ZfhzDzJPAgMBAAECggEAAoNSf3uDsM+0dorgIEM7sKdNh9TLYSBvFUaWjgUuDSwI
+0rpET6VN71/fn6Quhizx3ZUdb/Kcpw9mDnbGhdPEnnSS/+hkg52Iq+Zd/tYQZcd4
+0QqtDumb8xud0Li404k8YnBYcu5hnQBHS/vpAe4RXrlrG58vz8aLTsha0dl9eB+9
+H4wJ4apYAY3Jkmu26dcwGW39dF0IsWimClwi/+q7Ce4JVQ8JnJqjpW+45gVefXT7
+dd5a8OAOCUx68sSDfufgje8mC92B3OZiToGIvZ6rsvP9qyfUwImwLpeBjstXm4Ns
+oGa1bbdDPu0ubr81qm27d7XI6zlFI0x9bKa8pIndTQKBgQDch6Lt5wIFCHI4Ckcy
+MisyboO/q66+SxAsv/V46XKodSZPJmXypRz/miUfPnZuMnxSZgsekZQ9dly9QSpL
+9mHl8WC4crZdJ5chkSmciTiEemtSMJs/jJehXiY7SmDq9g5oLlG//NDzFb+JlisW
+UaounSTab+zkrricJkRT9vcb7QKBgQDFiVP0yLTrvEk2Us0u4ebI7blK00ejQi6n
+/YleUxPsVvayqfRXM9quulKZZmWau2SYAlRCr5CU1U6/T7e752YjbQikJWxC1hNK
+P87zyCtshhZHRpITsHbKlic141CDtPwWFF2E3qdq9bkdI2iwfiUHSlkF5fjzqxN3
+HRa4S0pXqwKBgGbQLZuwaXajO2z5DbxPO2hlsbK4fd1l7YKPdAgM/lGfXF7mTf1U
+ETLxUIFCg8BYdTGNNX3o7S3CcvYg3XFQys+DO06C3JaNkJ4rqTS7nMfWsxY8ZwS3
+rTGkTTqzMIAaexkDD0XsvhW2e/fPNQNQy4Cz7qyQJedvtc3G4XAr4YT5AoGAbItl
+ouCW6eJwqHUfYl+nnUljxAYNG1zGnnDnBvnHvNqcfNh+91v7EoA32ys1Ma0/PXgq
+LOqkG+SKfP2lDI++xECpuukFcESKHQarBbI8ikmz/D5/DmqtG+0eZrIeEAFndAqE
+yPpALLoRmj1WzYTDfBoSiPcxsVOUQqOtVF+q3jkCgYEAoCMdC7qULrQ/Yzr8gVP2
+jcFRiqzB13eEAF5HjIC6N8gO+Zj4mCvLVSULr4WNqNxabMlHKi3nAbqk2HmNaQ1L
+q2QZOrEh3qM7Z7RLwyyVI2INUNczlD9aU7gKwFglQtPPDHKrZha0xURsn8MRwRZI
+UPzds2xmpfHfdHIhco2q+tQ=
+-----END PRIVATE KEY-----`,
+  "client_email": "firebase-adminsdk-fbsvc@downloaderx-wa-code.iam.gserviceaccount.com"
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
